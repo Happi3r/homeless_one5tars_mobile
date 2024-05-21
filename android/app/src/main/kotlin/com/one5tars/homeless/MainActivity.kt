95710c901ec3b2d46ad55fa7989d269c0e95787e
Happi3r <dev.happi3r@gmail.com>
2024-05-21 12:25:22 +0900

    feat: add LoginService for OAuth Token

--- android/app/src/main/kotlin/com/one5tars/homeless/MainActivity.kt
+++ android/app/src/main/kotlin/com/one5tars/homeless/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.one5tars.homeless
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
