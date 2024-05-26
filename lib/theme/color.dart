c712153d120d109bfb3974ebbbeb01b5721a949a
jwleetop5 <jwleetop5@naver.com>
2024-05-26 23:53:09 +0900

    feat: add theme

---
+++ lib/theme/color.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class HomelessColor {
+  static const Color error = Color(0xFF2D142C);
+  static const Color onError = Color(0xFFEE4540);
+  static const Color success = Color(0xFF124E82);
+  static const Color onSuccess = Color(0xFFDAE1E9);
+  static const Color line = Color(0xFFCCCCCC);
+  static const Color text = Color(0xFF000000);
+  static const Color darkText = Color(0xFF464646);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color darkBackground = Color(0xFF2B2B2B);
+  static const Color empty = Color(0xFFAAAAAA);
+}
