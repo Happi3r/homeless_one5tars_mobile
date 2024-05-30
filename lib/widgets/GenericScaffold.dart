8bcfff2e3fcdb3cb7603259e8308d5af9ecac63a
jwleetop5 <jwleetop5@naver.com>
2024-05-31 00:45:58 +0900

    feat: update widgets

---
+++ lib/widgets/GenericScaffold.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:homeless/theme/spacing.dart';
+import 'package:homeless/widgets/Button.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
+import '../screens/signIn/InputUsage.dart';
+import '../theme/color.dart';
+import '../theme/textStyle.dart';
+import 'ContentTopSpacer.dart';
+
+class GenericScaffold extends StatelessWidget {
+  const GenericScaffold(
+      {super.key,
+      required this.appBarTitle,
+        required this.mainContent,
+      required this.appBarLeading,
+      required this.okButtonAction,
+      required this.content});
+
+  final String appBarTitle;
+  final String mainContent;
+  final Widget content;
+  final VoidCallback appBarLeading;
+  final VoidCallback okButtonAction;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          "사용 목적",
+          textAlign: TextAlign.start,
+          style: HomelessTextTheme.lg,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.78,
+        shadowColor: HomelessColor.text,
+        leading: IconButton(
+            onPressed: () {
+              appBarLeading();
+            },
+            icon: const Icon(Icons.chevron_left)),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Spacing.lg),
+          child: Column(
+            children: [
+              const ContentSpacer(),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  mainContent,
+                  textAlign: TextAlign.start,
+                  style: HomelessTextTheme.lg,
+                ),
+              ),
+              const SizedBox(height: Spacing.xl),
+              content,
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "*",
+                          textAlign: TextAlign.start,
+                          style: HomelessTextTheme.xxs
+                              .copyWith(color: HomelessColor.onError),
+                        ),
+                        Text(
+                          " 는 필수 입력 정보입니다.",
+                          textAlign: TextAlign.start,
+                          style: HomelessTextTheme.xxs
+                              .copyWith(color: theme.colorScheme.secondary),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "위의 데이터들은 더 나은 정보 제공을 위해 수집하는 목적입니다.",
+                          textAlign: TextAlign.start,
+                          style: HomelessTextTheme.xxs
+                              .copyWith(color: theme.colorScheme.secondary),
+                        )),
+                    const SizedBox(height: Spacing.sm),
+                    SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: HomelessTextButton(
+                            buttonText: "확인", buttonAction: okButtonAction))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
